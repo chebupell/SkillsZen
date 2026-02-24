4bf579048c01e4bd0a7fa8a1a8686eba627a49b3
EgorGer <QwertiQQQ@gmal.com>
2026-02-24 14:28:23 +0300

    feat: add heder component

---
+++ SkillsZen/src/pages/stats/components/header.tsx
@@ -0,0 +1,13 @@
+import '../stats.css'
+
+function Header() {
+  return (
+    <header className=" ">
+      <div className="logo">Img </div>
+      <h2 className="">SkillsZen</h2>
+      <button className="btn">log Out</button>
+    </header>
+  )
+}
+
+export default Header
