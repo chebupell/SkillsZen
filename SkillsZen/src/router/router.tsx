21502f4b756eb2716b58ca7c231c9d0a8e966c71
Tatsiana Hladkaya <tatyana.hladkaya@gmail.com>
2026-02-24 19:46:20 +0300

    feat: add structure to shared components

--- SkillsZen/src/router/router.tsx
+++ SkillsZen/src/router/router.tsx
@@ -13,7 +13,7 @@ export const router = createBrowserRouter([
     element: <App />,
     children: [
       {
-        path: '',
+        path: 'main',
         element: <MainPage />,
       },
       {
