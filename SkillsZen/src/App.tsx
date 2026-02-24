be746142bc6819d20af94e9cd638448d6a6ac427
Tatsiana Hladkaya <tatyana.hladkaya@gmail.com>
2026-02-24 18:01:52 +0300

    fix: fix Tailwind setup

--- SkillsZen/src/App.tsx
+++ SkillsZen/src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <div className="app-container">
-      <h1>SkillsZen</h1>
+      <h1 className="text-4xl font-bold text-blue-500 underline">SkillsZen</h1>
     </div>
   )
 }
