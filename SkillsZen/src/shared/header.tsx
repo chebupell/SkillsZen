676588ff40174d11066f6a4bfce6a0b61b2e516c
EgorGer <QwertiQQQ@gmal.com>
2026-03-03 10:51:15 +0300

    feat: add parameters in header

--- SkillsZen/src/shared/header.tsx
+++ SkillsZen/src/shared/header.tsx
@@ -1,19 +1,25 @@
 import logo from '../../public/fav.png'
 import CreateButton from './buttons/createButton'
 
-function Header() {
+type HeaderProps = {
+  buttonText: string
+  onButtonClick?: () => void
+  buttonDisabled?: boolean
+}
+
+function Header({ buttonText, onButtonClick, buttonDisabled = false }: HeaderProps) {
   return (
     <header className="flex items-center justify-between px-6 py-4 bg-white shadow-md">
       <div className="flex items-center gap-3">
         <img src={logo} alt="Logo" className="w-10 h-10 object-contain" />
-
         <h2 className="text-xl font-semibold text-gray-800">SkillsZen</h2>
       </div>
 
       <CreateButton
-        text="Log Out"
+        text={buttonText}
+        onClick={onButtonClick}
+        disabled={buttonDisabled}
         className="px-4 py-2 rounded-lg border hover:bg-gray-100"
-        onClick={() => console.log('Клац')}
       />
     </header>
   )
