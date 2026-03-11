e4a7900cba935bb46b86cc6ee7949efc86d21b4a
chebupell <115893102+chebupell@users.noreply.github.com>
2026-03-11 18:28:43 +0300

    feat: removing ml
    Co-authored-by: t-gladkaya <162370759+t-gladkaya@users.noreply.github.com>

--- SkillsZen/src/pages/menuPage/components/exerciseCard.tsx
+++ SkillsZen/src/pages/menuPage/components/exerciseCard.tsx
@@ -11,7 +11,7 @@ export default function ExerciseCard({ taskIcon, title, descriprion }: ExerciseC
     return (
         <Card className="flex justify-center w-64 align-center flex-col">
             <div
-              className="h-30 bg-contain bg-center bg-no-repeat mg-50"
+              className="h-32 w-32 bg-cover bg-center bg-no-repeat mb-5 rounded-2xl overflow-hidden shadow-sm"
               style={taskIcon ? { backgroundImage: `url('/icons/${taskIcon}')` } : {}}
             />
             <CardTitle className="text-2xl text-center">{title}</CardTitle>
