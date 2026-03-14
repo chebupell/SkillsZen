76d900ac12b1c36f0be894b676f3a1f56dd054e4
t-gladkaya <162370759+t-gladkaya@users.noreply.github.com>
2026-03-14 18:35:01 +0300

    feat: add training pages for JS, TS, and Algorithms (#28)
    * style: add background to exercise pages
    
    * feat: implement basic layout for exercise pages
    
    * style: add icons to exercises blocks
    
    * style: place cards elements in the right order
    
    * feat: add retryTag and successTag
    
    * feat: add progress bar
    
    * style: align elements according to layout
    
    * feat: connect exercises pages with API
    
    * style: update flags appearance
    
    * Merge changes from development with exercises page
    
    * fix: connect API with right auth firebase key
    
    * chore: update old paths
    
    * feat: implement backButton
    
    * fix: prevent overlap between page title and back button on small screens
    
    * chore: move back button styles to variants

--- SkillsZen/vite.config.ts
+++ SkillsZen/vite.config.ts
@@ -12,6 +12,14 @@ export default defineConfig({
     }),
     tailwindcss(),
   ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
