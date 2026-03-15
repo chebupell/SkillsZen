bc927dd2791532c5115d98baa2050245f824791d
Tatsiana Hladkaya <tatyana.hladkaya@gmail.com>
2026-03-15 20:32:19 +0300

    style: add inner border to startTag

--- SkillsZen/src/pages/exercises/tags/startTag.tsx
+++ SkillsZen/src/pages/exercises/tags/startTag.tsx
@@ -2,10 +2,12 @@ import React from "react";
 
 const StartTag: React.FC = () => {
   return (
-    <div className="gap-4 rounded-xl items-center border-2 border-yellow-200 bg-yellow-100 p-2 px-6 min-w-40">
-      <div className="flex items-center gap-3 text-black-900 text-center">
-        <div className="rounded-full border-3 border-yellow-200 w-4 h-4"></div>
-        Not Started
+    <div className="relative inline-flex rounded-full items-center bg-yellow-100 py-2 px-6 min-w-40">
+      <div className="pointer-events-none absolute inset-1 rounded-full border-2 border-yellow-300" />
+      <div className="flex items-center justify-center gap-3 text-black-900 text-center">
+
+        <div className="rounded-full border-2 border-yellow-300 w-4 h-4"></div>
+        <span>Not Started</span>
       </div>
     </div>
   )
