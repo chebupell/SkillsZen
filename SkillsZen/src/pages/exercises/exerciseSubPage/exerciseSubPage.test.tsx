ca4194fcfae2b079fb045cdd389b3bf22b8bef8c
Tatsiana Hladkaya <tatyana.hladkaya@gmail.com>
2026-03-15 22:39:07 +0300

    feat: add test to ensure correct topic title on exercise page

---
+++ SkillsZen/src/pages/exercises/exerciseSubPage/exerciseSubPage.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react';
+import { expect, it, describe } from 'vitest';
+import { ExerciseSubPage } from "./exerciseSubPage";
+import type { ExerciseStatus, ExerciseSubPageProps } from '../../../types/exerciseTypes';
+import { BrowserRouter } from 'react-router-dom';
+
+describe('ExerciseSubPage', () => {
+  const mockProps: ExerciseSubPageProps = {
+    topicImg: '/test-img.png',
+    topicTitle: 'Test Topic Title',
+    statusText: 'test 5 questions',
+    exercisesProgress: '1/5 completed',
+    exercises: [
+      { id: '1', title: 'exercise 1', status: 'completed' as ExerciseStatus}
+    ],
+  };
+
+  it ('should render topic title correctly', () => {
+    render(
+      <BrowserRouter>
+        <ExerciseSubPage {...mockProps} />
+      </BrowserRouter>
+    );
+    expect(screen.getByText('Test Topic Title')).toBeInTheDocument();
+  });
+})
