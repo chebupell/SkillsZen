75726e5669c62cf05985876218cab90702d7c858
Tatsiana Hladkaya <tatyana.hladkaya@gmail.com>
2026-03-22 19:24:15 +0300

    style: style exercise page for middle screens

--- SkillsZen/src/pages/exercises/exerciseSubPage/exerciseSubPage.tsx
+++ SkillsZen/src/pages/exercises/exerciseSubPage/exerciseSubPage.tsx
@@ -47,33 +47,33 @@ export const ExerciseSubPage: React.FC<ExerciseSubPageProps> = ({
 
   return (
     <div className="p-4 sm:p-10 min-h-screen">
-      <div className="grid grid-cols-[auto_1fr] sm:grid-cols-[auto_1fr_auto] items-center gap-2 mb-6">
+      <div className="grid grid-cols-[auto_1fr] sm:grid-cols-[auto_1fr_auto] items-center gap-2 mb-3">
         <div className="flex justify-start">
-            <BackButton />
+          <BackButton />
         </div>
-        <div className="text-2xl sm:text-4xl text-center">{topicTitle}</div>
+        <div className="text-2xl sm:text-4xl text-right sm:text-center">{topicTitle}</div>
         <div className="hidden sm:block w-25" aria-hidden="true"></div>
       </div>
 
-      <p className="text-center text-gray-600 mb-8">{exercisesProgress} completed blocks</p>
+      <p className="text-right sm:text-center text-gray-600 mb-4">{exercisesProgress} completed blocks</p>
 
       <div className="grid gap-4 md:grid-cols-1 cursor-pointer">
         {exercises.map((item) => (
           <div
             key={item.id}
-            className="p-4 bg-white rounded-xl shadow-lg md:flex items-center gap-8 hover:bg-gray-100"
+            className="p-4 bg-white rounded-xl shadow-lg flex flex-col md:flex-row items-center justify-center md:justify-between gap-4 md:gap-8 hover:bg-gray-100"
             onClick={() => handleExerciseClick(item.id, item.status)}
           >
-            <div className="flex items-center gap-4 mb-1">
+            <div className="flex flex-row items-center justify-center md:justify-start gap-4 w-full md:w-auto">
               <img src={topicImg} alt="" className="max-h-10 rounded-lg" />
-              <div>
+              <div className="text-left">
                 <h3 className="font-semibold text-lg">{item.title}</h3>
                 <p className="text-sm text-gray-500">
                   {item.totalQuestions} questions
                 </p>
               </div>
             </div>
-            <div className="ml-auto">{STATUS_TAGS[item.status] || <StartTag />}</div>
+            <div className="mt-2 md:mt-0 md:ml-auto">{STATUS_TAGS[item.status] || <StartTag />}</div>
           </div>
         ))}
       </div>
