08ab5530ef34aedb10840b5de7b4be9486b4f2c1
chebupell <kremenvov11@gmail.com>
2026-04-02 20:13:42 +0300

    feat: add page

--- SkillsZen/src/pages/exercises/blockResultPage/blockResultPage.tsx
+++ SkillsZen/src/pages/exercises/blockResultPage/blockResultPage.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import PageLayout from '../../../components/shared/PageLayout'
 import ToMenuButton from '../../../components/shared/toMenuButton'
+import { useLocation } from 'react-router-dom'
+
+interface ResultProps {
+  block_id: string
+  total_questions: number
+  correct_count: number
+}
+
 const BlockResultPage: React.FC = () => {
+
+  const { state } = useLocation()
+  const { block_id, total_questions, correct_count } = (state || {}) as ResultProps
+
   return (
     <PageLayout backgroundImage="main-page-background.png">
       <ToMenuButton />
-      <h2 className="text-2xl sm:text-4xl text-center m-4">Your results</h2>
+      <div>Your results</div>
+      <div>Topic: {block_id}</div>
+      <div>{correct_count}/{total_questions}</div>
     </PageLayout>
   )
 }
