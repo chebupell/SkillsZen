633d8eb1cd4b9053f4847d1a5f41d3ea9e735676
chebupell <kremenvov11@gmail.com>
2026-04-05 21:18:23 +0300

    feat: some text to results

--- SkillsZen/src/pages/exercises/blockResultPage/blockResultPage.tsx
+++ SkillsZen/src/pages/exercises/blockResultPage/blockResultPage.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import PageLayout from '../../../components/shared/PageLayout.tsx'
 import ToMenuButton from '../../../components/shared/toMenuButton'
 import { useLocation } from 'react-router-dom'
+import { Card } from '../../../components/ui/card.tsx'
 
 interface ResultProps {
   block_id: string
@@ -13,13 +14,23 @@ const BlockResultPage: React.FC = () => {
 
   const { state } = useLocation()
   const { block_id, total_questions, correct_count } = (state || {}) as ResultProps
+  
+  const getFeedback = (score:number) => {
+    if (score >= 90) return "Ты — легенда! 🏆";
+    if (score >= 70) return "Блестяще! 🔥";
+    if (score >= 50) return "Неплохо для начала 👍";
+    return "Есть куда расти 🌱";
+  };
 
   return (
     <PageLayout backgroundImage="main-page-background.webp">
       <ToMenuButton />
-      <div>Your results</div>
-      <div>Topic: {block_id}</div>
-      <div>{correct_count}/{total_questions}</div>
+      <Card className='mt-20 flex items-center justify-center'>
+      <div className='text-4xl text-secondary-foreground mb-10 max-w-100'>Your results</div>
+      <div className='text-4xl text-secondary-foreground mb-10 max-w-100'>Topic: {block_id}</div>
+      <div className='text-4xl text-secondary-foreground mb-10 max-w-100'>{Math.floor(correct_count/total_questions*100)}%</div>
+      <div className='text-4xl text-secondary-foreground mb-10 max-w-100'>{getFeedback(Math.floor(correct_count/total_questions*100))}</div>
+      </Card>
     </PageLayout>
   )
 }
